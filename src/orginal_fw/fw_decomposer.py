f251f19bbc874b1396f08772308da365289b0cce
Andrej Antunovikj <andrej@case.edu>
2023-07-19 00:08:52 +0200

    Add 0xc0 empty section over the CDCSS tones

--- src/orginal_fw/fw_decomposer.py
+++ src/orginal_fw/fw_decomposer.py
@@ -11,16 +11,23 @@ class FwDecompozer:
       self.file.seek(self.vector_table_address, 0)
       output.write(self.file.read(self.vector_table_size))
       output.close()
-
+# After the vector table, the three parts are: the bootloader, the firmware up to dfb4 and the firmware from e074 onwards.
    def save_part1(self, filename):
       output = open(filename, 'wb')
       self.file.seek(0)
-      output.write(self.file.read(self.vector_table_address))
+      output.write(self.file.read(self.vector_table_address)) # this would write the first 4096 bytes (the bootloader)
       output.close()
 
    def save_part2(self, filename):
       output = open(filename, 'wb')
-      self.file.seek(self.vector_table_address + self.vector_table_size, 0)
+      self.file.seek(self.vector_table_address + self.vector_table_size, 0) # this would be 48+4096 bytes in
+      # Read the next dfb4 bytes (this is the firmware up to dfb4)
+      output.write(self.file.read(0xdfb4))
+      output.close()
+   
+   def save_part3(self, filename):
+      output = open(filename, 'wb')
+      self.file.seek(0xe074, 0) # this would be 57300 bytes in
       output.write(self.file.read())
       output.close()
 
@@ -30,3 +37,4 @@ if __name__ == '__main__':
    fw.save_vector_table(args[4])
    fw.save_part1(args[5])
    fw.save_part2(args[6])
+   fw.save_part3(args[7])
